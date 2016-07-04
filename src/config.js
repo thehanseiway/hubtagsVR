292ea70a8ad09d46abaa613056a42d540cb80ac1
wladislaw <thehanseiway@gmail.com>
2016-07-04 08:39:23 +0300

    Update config

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@ const config = {
         "client_id": '6147d0e9f59196aed457',
     },
     "hubtagsvr.surge.sh": {
-        "authUrl": 'https://hubtagsvr.surge.sh/auth/callback',
+        "authUrl": 'https://hubtagsvr-prod.herokuapp.com/auth/callback',
         "client_id": '8e64b55dd8e15c1392d7',
     },
 }[location.hostname]
