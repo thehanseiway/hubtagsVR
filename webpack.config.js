3684efa3449dfc2acbb06cfd1aa26f058d4dfe02
VladislavRoscovan <thehanseiway@gmail.com>
2016-07-06 14:05:09 +0300

    Finish lessons

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,20 @@
+require('babel/register');
 var webpackConfig = require('hjs-webpack');
+var React = require('react');
+var PublicPage = require('./src/pages/public');
+var Layout = require('./src/pages/layout');
 
 module.exports = webpackConfig({
   in: 'src/app.js',
   out: 'public',
   clearBeforeBuild: true,
   html: function (context) {
+    const publicPage = React.renderToString(React.createElement(PublicPage));
+    const layoutPage = React.renderToString(React.createElement(Layout, {me: {}}));
+
     return {
-        'index.html': context.defaultTemplate(),
-        '200.html': context.defaultTemplate(),
+        'index.html': context.defaultTemplate({html: publicPage}),
+        '200.html': context.defaultTemplate({html: layoutPage}),
     }
   }
 });
